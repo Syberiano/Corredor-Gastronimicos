c5a38eb054d9c0a00618ac98df756cf731f4fd34
Syberiano <syberiano.2704@gmail.com>
2025-05-12 21:52:49 -0500

    Buscar

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 function initMap() {
     let mapa = new google.maps.Map(document.getElementById("mapa"), {
-        center: { lat: 6.2442, lng: -75.5812 }, // Medellín por defecto
+        center: { lat: 6.2442, lng: -75.5812 }, 
         zoom: 12
     });
 
-    window.mapa = mapa; // Guarda el mapa globalmente para futuras actualizaciones
+    window.mapa = mapa; 
 }
 
 function actualizarMapa() {
     let ubicaciones = {
-        1: { lat: 6.2085, lng: -75.5670 }, // Poblado
-        2: { lat: 6.1759, lng: -75.5917 }, // Envigado
-        3: { lat: 6.1516, lng: -75.6152 }, // Sabaneta
-        4: { lat: 6.1578, lng: -75.6353 }, // La Estrella
-        5: { lat: 6.1691, lng: -75.6114 }, // Itagüí
-        6: { lat: 6.3373, lng: -75.5610 }, // Bello
-        7: { lat: 6.3467, lng: -75.4357 }, // Copacabana
-        8: { lat: 6.4378, lng: -75.3341 }, // Barbosa
-        9: { lat: 6.3753, lng: -75.4444 }, // Girardota
-        10: { lat: 6.0329, lng: -75.6344 }, // Caldas
-        11: { lat: 6.2442, lng: -75.5812 }  // Medellín
+        1: { lat: 6.2085, lng: -75.5670 }, 
+        2: { lat: 6.1759, lng: -75.5917 }, 
+        3: { lat: 6.1516, lng: -75.6152 },
+        4: { lat: 6.1578, lng: -75.6353 }, 
+        5: { lat: 6.1691, lng: -75.6114 }, 
+        6: { lat: 6.3373, lng: -75.5610 }, 
+        7: { lat: 6.3467, lng: -75.4357 }, 
+        8: { lat: 6.4378, lng: -75.3341 }, 
+        9: { lat: 6.3753, lng: -75.4444 }, 
+        10: { lat: 6.0329, lng: -75.6344 }, 
+        11: { lat: 6.2442, lng: -75.5812 } 
     };
+
+   
+    let linkMapaPoblado = document.getElementById("mapaPoblado");
+    if (linkMapaPoblado) {
+        linkMapaPoblado.addEventListener("click", () => {
+            document.getElementById("buscar_zona").value = "1"; 
+        });
+    }
 
     let restaurantes = {
         1: [
@@ -50,7 +58,7 @@ function actualizarMapa() {
             icon: "https://maps.google.com/mapfiles/ms/icons/red-dot.png"
         });
 
-        // Agregar marcadores de restaurantes en la zona seleccionada
+      
         if (restaurantes[seleccion]) {
             restaurantes[seleccion].forEach(restaurante => {
                 let marcador = new google.maps.Marker({
